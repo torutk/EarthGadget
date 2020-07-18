aa1053903ebeb1a8172e57e85f781545f918e207
TAKAHASHI,Toru <torutk@gmail.com>
2020-07-18 22:00:17 +0900

    Migrate to gradle and JPMS module

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/*
+ *  Copyright © 2020 TAKAHASHI,Toru
+ */
+
+module com.torutk.gadget.earth {
+    requires com.torutk.gadget.support;
+    requires javafx.controls;
+    opens com.torutk.gadget.earth to javafx.graphics;
+}
